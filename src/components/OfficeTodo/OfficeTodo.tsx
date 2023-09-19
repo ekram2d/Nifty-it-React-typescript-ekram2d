09ffbaadabe751bc2536f4f798ce1886d38330a5
Ekram Hossain <ekramhossain117@gmail.com>
2023-09-19 09:11:57 +0600

    feat: [ADD]=office todo task management functionality added

--- src/components/OfficeTodo/OfficeTodo.tsx
+++ src/components/OfficeTodo/OfficeTodo.tsx
@@ -1,67 +1,126 @@
-import { useState } from "react";
-import { Data, Inputs } from "../Interface/interfacedata";
+import { useReducer } from "react";
+// import { Data, Inputs } from "../Interface/interfacedata";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { Data, Inputs, OfficeTodoAction, State } from "../Interface/interfaceAll";
+
+const initialState: State = {
+  data: [
+    {
+      name: "project1",
+      id: 1,
+      date: "1-01-2023",
+      idModalOpen: false,
+      ModalTest: "",
+    },
+  ],
+};
+
+const reducer = (state: State, action: OfficeTodoAction) => {
+  if (action.type === "add") {
+    const allTasks = [...state.data, action.payload];
+
+    return {
+      ...state,
+      data: allTasks,
+    };
+  }
+
+  // Your reducer logic here
+  return state; // Make sure to return the state if no action type matches
+};
 
 
 const OfficeTodo = () => {
-  const [data, setData] = useState<Data[]>([]);
+  const [tasks, dispatch] = useReducer(reducer, initialState);
 
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<Inputs>();
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    
-    console.log(data);
+    const newTask: Data = {
+      id: new Date().getTime(),
+      name: data.textList,
+      date: data.submissionDate,
+      idModalOpen: false,
+      ModalTest: "",
+    };
+
+    dispatch({ type: "add", payload: newTask });
+    reset();
   };
+
 
   return (
     <>
-      <div className="text-center font-bold text-2xl ">Please Enter The Office Task</div>
-
-
+      <div className="w-[60%] mx-auto mt-10 text-center text-2xl">
+        <h1 className="text-3xl uppercase font-serif">Task List</h1>
+        <table className="w-full mt-5">
+          <thead>
+            <tr>
+              <th className="text-2xl text-green-800 font-bold uppercase p-3">
+                Task Name
+              </th>
+              <th className="text-2xl text-green-800 font-bold uppercase p-3">
+                Submission Date
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks?.data?.map((task) => (
+              <tr key={task.id}>
+                <td className="p-3">{task.name}</td>
+                <td className="p-3">{task.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <div className="w-[60%] mt-4 mx-auto shadow-2xl ">
-      <ul>
-            <li>projcet1 : <span>Id : 1</span></li>
-      </ul>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="w-[90%] flex justify-center items-center pt-2 ">
-          <input
-            className="bg-black text-white p-4 font-bold text-center rounded-xl shadow-2xl  pt-4 w-[40%] mx-auto "
-            placeholder="please enter your task"
-            defaultValue=""
-            {...register("textList")}
-          />
-          {errors.textList && (
-            <span className="text-red-500">This field is required</span>
-          )}
-        </div>
+        <h1 className="text-center font-bold text-2xl ">
+          Please Enter The Office Task
+        </h1>
 
-        <br />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="w-[90%] flex justify-center items-center pt-2 ">
+            <input
+              className="bg-black text-white p-4 font-bold text-center rounded-xl shadow-2xl  pt-4 w-[40%] mx-auto "
+              placeholder="please enter your task"
+              defaultValue=""
+              {...register("textList", { required: true })}
+            />
+            {errors.textList && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
 
-    <div className="w-[90%] flex justify-center items-center ">
-    <input
-          className="bg-black text-white p-4 fk font-bold text-center rounded-xl shadow-2xl  pt-2 w-[40%] mx-auto "
-         
-          placeholder="Enter ID"
-          {...register("id", { required: true })}
-        />
-        {errors.id && <span className="text-red-500">This field is required</span>}
-        <br />
-    </div>
-<div className="w-[90%] flex justify-center items-center">
-      
-<button
-          className="bg-red-300 hover:bg-red-400 text-white rounded-md p-2 mt-5 "
-          type="submit"
-        >
-          Submit
-        </button>
-</div>
-      </form>
-    </div>
+          <br />
+
+          <div className="w-[90%] flex justify-center items-center">
+            <input
+              type="date"
+              className="bg-black text-white p-4 fk font-bold text-center rounded-xl shadow-2xl  pt-2 w-[40%] mx-auto "
+              placeholder="Enter Submission Date"
+              {...register("submissionDate", { required: true })}
+            />
+            {errors.submissionDate && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+
+          <div className="w-[90%] flex justify-center items-center">
+            <button
+              className="bg-red-300 hover:bg-red-400 text-white rounded-md p-2 mt-5"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
